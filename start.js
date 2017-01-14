7527f407005fba1990533da4573d9f9b9816711e
Tomasz <tomasz.szawara@gmail.com>
2017-01-14 11:41:09 +0000

    switched from chromedriver to selenium-drivers

--- start.js
+++ start.js
@@ -1,4 +1,11 @@
-require('./tools/initAssert');
-require('./tools/initWebDriver');
-require('./tools/initUtils');
-require('./tools/runMocha');
+var seleniumDrivers = require('selenium-drivers');
+
+seleniumDrivers.init({
+    browserName: 'chrome',
+    download: true
+}).then(function () {
+    require('./tools/initAssert');
+    require('./tools/initWebDriver');
+    require('./tools/initUtils');
+    require('./tools/runMocha');
+});
--- tools/initWebDriver.js
+++ tools/initWebDriver.js
@@ -1,5 +1,3 @@
-require('chromedriver');
-
 global.webdriver = require('selenium-webdriver');
 global.By = webdriver.By;
 global.until = webdriver.until;
